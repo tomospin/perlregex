aaf8b0550054d7b416c709f8b46354a0b5291044
sirapat <tomospin@gmail.com>
2018-03-18 15:07:37 +0700

    first commit

---
+++ 57070503438.pl
@@ -0,0 +1,14 @@
+# A simple program to move and change file path/name for a purpose
+# Written by Sirapat Na Ranong 57070503438
+# 18 March 2018
+
+
+$directory = '/Users/sirapat/Desktop/tmp/mp3/playlists';
+opendir (DIR, $directory) or die "ERROR while opening $directory\n";
+
+
+while ($file = readdir(DIR)) {
+	print()
+}
+
+foreach
