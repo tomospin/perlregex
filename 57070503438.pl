295e013b69314e09e021be5177ca78df789f4c39
sirapat <tomospin@gmail.com>
2018-03-19 02:17:01 +0700

    final

--- 57070503438.pl
+++ 57070503438.pl
@@ -1,7 +1,6 @@
 # A simple program to move and change file path/name for a purpose
 # Written by Sirapat Na Ranong 57070503438
 # 18 March 2018
-
 use File::Path qw(make_path);
 use File::Copy;
 
@@ -25,15 +24,15 @@ foreach $file (@files) {
 	print "Song's old path: $path\n";
 	$path =~ s/\/cd-lib\/mp3/music/;  # change /cd-lib/mp3 to music
 	$path =~ s/[^\/]*\.mp3/files/;    # change 000-songname.mp3 to files (regex pattern = anything not / followed by .mp3)
-	$path =~ s/_//;					  # remove _ of subdirectory ex. Dixie_Chicken to DixieChicken
+	$path =~ s/_//;	# remove _ of subdirectory ex. Dixie_Chicken to DixieChicken
 	print "Song's new path: $path\n";
 
 	$playlistpath = $path;		
-	$playlistpath =~ s/[^\/]*\/files//;  			# ex. music/rock/LittleFeat/DixieChicken/files returns music/rock/LittleFeat/
+	$playlistpath =~ s/[^\/]*\/files//;  # ex. music/rock/LittleFeat/DixieChicken/files returns music/rock/LittleFeat/
 	print "Playlist's new path: $playlistpath\n";
 	$playlistname0 = $path =~ m/\/([^\/]*)\/files/;	# ex. music/rock/LittleFeat/DixieChicken/files returns DixieChicken
 	$playlistname0 = $1;	
-	$playlistname = $1 . "\.m3u"; 					# concatenate DixieChicken with .m3u
+	$playlistname = $1 . "\.m3u"; # concatenate DixieChicken with .m3u
 	print "Playlist's new name: $playlistname\n";
 
 	if (!-d $path) {	      
@@ -42,13 +41,12 @@ foreach $file (@files) {
 		print "Directory $path exists\n";
 	}
 
-	#move($file, $playlistpath . $playlistname);
 	open(DATA, '>' , $playlistpath . $playlistname) or die "Could not create $playlistname\n";
 
 	foreach $line (@lines) {  # each line of song inside playlist
-		$line =~ s/\n//;		# path of a song
+		$line =~ s/\n//;	# path of a song
 		$songname = $line =~ m/[^\/]*\.mp3/;	# extract song name from path
-		$songname = $&;							#
+		$songname = $&;	
 		$songname =~ s/.mp3/.mpg/;
 		if (-e $line) {
 			print("Moving $line\n");
